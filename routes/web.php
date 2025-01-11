2d6da29b335f912297446e584a09054262e8aa14
aliimrannn <aliimran9703@gmail.com>
2025-01-12 01:19:03 +0800

    Route configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResearchGrantController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\MilestoneController;
+use App\Http\Controllers\AcademicianController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Research Grant Routes
+Route::resource('researchgrants', ResearchGrantController::class);
+
+// Milestone Routes
+Route::resource('milestones', MilestoneController::class);
+
+// Member Routes
+Route::resource('members', MemberController::class);
+
+// Academician Routes
+Route::resource('academicians', AcademicianController::class);
